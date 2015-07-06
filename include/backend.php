15e0164a921cb2f818917c75ceaefe0c0390cd7c
Cameron Moreau <cameron.cm6@gmail.com>
2015-07-05 22:26:55 -0500

    Screw services..

---
+++ include/backend.php
@@ -0,0 +1,79 @@
+<?php
+header('Content-Type: application/json');
+define('FILE_IMAGE', 1);
+define('FILE_VIDEO', 2);
+
+/* Get categories and drop dir listings */
+$categories = scandir('Categories');
+$categories = array_splice($categories, 2);
+
+/* Category was not set */
+if(!isset($_GET['category'])) {
+	echo json_encode(array(
+		'categories' => $categories
+	));
+} 
+
+/* List images of selected cateogry */
+else {
+	$currentCategory = $_GET['category'];
+
+	/* Make requested exists in the categories */
+	if(in_array($currentCategory, $categories)) {
+		$images = getImagesFromDir('Categories/'.$currentCategory);
+		echo json_encode(array(
+			'data' => $images
+		));
+	} else {
+		echo json_encode(array(
+			'error' => 'No category found'
+		));
+	}
+}
+
+/* Strips out everything not an image or video from scandir */
+function getImagesFromDir($path) {
+	$dataImages = array();
+
+	$images = scandir($path);
+	foreach ($images as $image) {
+		$imagePath = $path.'/'.$image;
+		$type = getFileType($imagePath);
+
+		if($type != 0) {
+			array_push($dataImages, array(
+				'path' => '/include/'.$imagePath,
+				'type' => fileTypeToString($type)
+			));
+		}
+	}
+
+	return $dataImages;
+}
+
+/* Returns string for json use */
+function fileTypeToString($type) {
+	switch ($type) {
+		case FILE_IMAGE:
+			return 'image';
+
+		case FILE_VIDEO:
+			return 'video';
+		
+		default:
+			return 0;
+	}
+}
+
+/* Returns defined integer for php use */
+function getFileType($file) {
+	$mime = mime_content_type($file);
+
+	if(strstr($mime, 'video/')) {
+		return FILE_VIDEO;
+	} else if(strstr($mime, 'image/')) {
+		return FILE_IMAGE;
+	}
+
+	return false;
+}
--- js/app.js
+++ js/app.js
@@ -6,8 +6,13 @@ app.config(function($routeProvider, $locationProvider) {
 			templateUrl: 'templates/intro.html',
 			controller: 'IntroController'
 		})
-		.when('/select', {
-			templateUrl: 'templates/category-select.html'
+		.when('/categories', {
+			templateUrl: 'templates/category-select.html',
+			controller: 'CategorySelectController'
+		})
+		.when('/category/:category', {
+			templateUrl: 'templates/category.html',
+			controller: 'CategoryController'
 		})
 		.otherwise({
 			redirectTo: '/'
@@ -17,12 +22,83 @@ app.config(function($routeProvider, $locationProvider) {
 	//$locationProvider.html5Mode(true);
 });
 
+app.factory('LocalData', function($http, $q) {
+	var LocalData = {};
+	var deffered = $q.defer();
+	var data = [];
+
+	LocalData.getCategories = function() {
+		data = []
+		$http.get('../include/backend.php')
+			.success(function(d) {
+				console.log(d.categories);
+				data = d.categories;
+				deffered.resolve();
+			})
+		return deffered.promise;
+	};
+
+	LocalData.getContents = function(category) {
+		data = [];
+		$http.get('../include/backend.php?category='+category)
+			.success(function(d) {
+				console.log(d.data);
+				data = d.data;
+				deffered.resolve();
+			})
+		return deffered.promise;
+	};
+
+	LocalData.data = function() {
+		var d = data;
+		data = [];
+		return d;
+	};
+
+	return LocalData;
+});
+
 app.controller('IntroController', function($scope, $location, Fullscreen) {
 	$scope.isFullScreen = false;
 
 	$scope.start = function() {
-  		if (Fullscreen.isEnabled()) {
+  		/*if (Fullscreen.isEnabled()) {
      		$location.path('select');
-  		} else Fullscreen.all(); //Start fullscreen
+  		} else Fullscreen.all(); //Start fullscreen*/
+  		$location.path('categories');
 	};
 });
+
+app.controller('CategorySelectController', function($scope, $location, LocalData, $http) {
+	$scope.categories = [];
+
+	$http.get('../include/backend.php')
+		.success(function(data) {
+			$scope.categories = data.categories
+		});
+
+	/*LocalData.getCategories().then(function() {
+		$scope.categories = LocalData.data();
+	});*/
+});
+
+app.controller('CategoryController', function($scope, $routeParams, LocalData, $http) {
+	$scope.selectedCategory = $routeParams.category;
+	$scope.items = [];
+
+	/*LocalData.getContents($scope.selectedCategory).then(function() {
+		$scope.items = LocalData.data();
+	});*/
+
+	$http.get('../include/backend.php?category='+$scope.selectedCategory)
+		.success(function(data) {
+			$scope.items = data.data;
+		});
+});
+
+
+
+
+
+
+
